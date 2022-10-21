34da51f5f1a146be131697d4007085eb049abd6f
unknown <you@example.com>
2022-10-21 00:03:13 -0500

    ObteniendoDatosDeRegistro6

---
+++ JS/registro.js
@@ -0,0 +1,26 @@
+//declaracion de variables de referencia a objetos
+const nombre=document.getElementById('name');
+const email=document.getElementById('email');
+const user=document.getElementById('user');
+const pass=document.getElementById('pass');
+const formulario=document.getElementById('formRegistro');
+
+//generando eventos
+formulario.addEventListener('submit', Registro);
+
+function Registro(e){
+    e.preventDefault();
+
+    let nombreVal=nombre.value;
+    let emailVal=email.value;
+    let userVal=user.value;
+    let passValue=pass.value;
+
+    if(nombreVal=='' || emailVal=='' || userVal=='' || passValue==''){
+        return;
+    }
+
+    console.log('validando datos');
+
+}
+
